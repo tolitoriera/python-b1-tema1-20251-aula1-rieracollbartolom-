599c93ef935b5f9682363aea95b9a6ab71560904
tolitoriera <82359751+tolitoriera@users.noreply.github.com>
2026-03-20 19:28:19 +0100

    Update ej1a2.py

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -42,13 +42,16 @@ Exemple:
 '''
 
 def sum_odd_numbers(list_numbers):
-    total = 0
-  for n in list_numbers:
-    if n < 0:
-      raise ValueError("Error!!La lista contiene valores menores que cero")
-    else:
-      total += n
-  return total
+    add = 0
+    for i in list_numbers:
+        if not isinstance(i, int):
+            raise ValueError("Numbers in the list must be integer.")
+        elif i < 0:
+            raise ValueError("Numbers in the list must be greater\
+                             or equal than 0.")
+        elif i %2 != 0:
+            add += i
+    return add
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
