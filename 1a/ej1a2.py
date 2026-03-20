b67d84c13089a7088391a2749d12b47046dc64f0
tolitoriera <82359751+tolitoriera@users.noreply.github.com>
2026-03-20 19:12:13 +0100

    Update ej1a2.py

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -42,9 +42,14 @@ Exemple:
 '''
 
 def sum_odd_numbers(list_numbers):
-    # Write here your code
-    pass
+    total = 0
+  for n in list_numbers:
+    if n < 0:
+      raise ValueError("Error!!La lista contiene valores menores que cero")
+    else:
+      total += n
+  return total
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
+print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
