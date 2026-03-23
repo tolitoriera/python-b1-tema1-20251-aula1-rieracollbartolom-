50b09b20a875ab07c01f12b61947e7bbd88d33aa
tolitoriera <82359751+tolitoriera@users.noreply.github.com>
2026-03-23 09:30:55 +0100

    Update ej1c4.py

--- 1c/ej1c4.py
+++ 1c/ej1c4.py
@@ -41,7 +41,10 @@ Exemple:
 
 
 def is_palindrome(word):
-    # Write here your code
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome(word[1:-1])
     pass
 
 
