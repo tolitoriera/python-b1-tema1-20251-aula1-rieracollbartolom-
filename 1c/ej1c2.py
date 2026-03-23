0cb51dbdc79a89719c833413ffbff62aa616f8ca
tolitoriera <82359751+tolitoriera@users.noreply.github.com>
2026-03-23 09:34:40 +0100

    Update ej1c2.py

--- 1c/ej1c2.py
+++ 1c/ej1c2.py
@@ -46,4 +46,4 @@ def invert_list(lst):
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # lst = [1, 20, 3, 40, 5]
-#print(invert_list(lst))
+print(invert_list(lst))
