900b201f8c8f6bb201ce3e6814956319996776ec
tolitoriera <82359751+tolitoriera@users.noreply.github.com>
2026-03-23 09:23:41 +0100

    Update ej1c3.py

--- 1c/ej1c3.py
+++ 1c/ej1c3.py
@@ -38,11 +38,14 @@ Exemple:
 
 
 def find_max(lst):
-    # Write here your code
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return max(lst[0], find_max(lst[1:]))
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # numbers_list = [1, 5, 2, 7, 3]
-# print(find_max(numbers_list))
+print(find_max(numbers_list))
